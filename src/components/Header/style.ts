91916f8c787841b932263ac2dddf8eda5db5c33c
Natália de Medeiros Costa <70599062+Nataliam32@users.noreply.github.com>
2023-05-04 10:15:15 -0300

    feature: header

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,18 +1,7 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
-  .loginHeader {
-    height: 72px;
-    width: 295.8px;
-    padding: 10px;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    margin: 0 auto;
-  }
-
+  
   .registerHeader {
     height: 72px;
     width: 90%;
@@ -23,6 +12,7 @@ export const StyledHeader = styled.header`
     align-items: center;
 
     margin: 0 auto;
+
   }
 
   .homeHeader {
@@ -35,23 +25,102 @@ export const StyledHeader = styled.header`
     align-items: center;
 
     margin: 0 auto;
+
   }
 
-  .backPageBtn,
-  .logoutBtn {
-    display: inline-flex;
+  .header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 1rem;
+
+  background-color: rgb(5, 5, 5);
+  color: #fff;
+
+  }
+
+  .header-left {
+    display: flex;
     align-items: center;
+
+  }
+
+  .header-right {
+    display: flex;
+    flex-direction: column;
     justify-content: center;
 
-    height: 32px;
-    width: 79.5px;
+  }
 
-    border-radius: 4px;
-    padding: 0px 16px 0px 16px;
+  .header-item {
+    margin: 0.5rem 0;
+    color: var(--color-primary);
 
-    color: var(--grey-0);
-    font-size: 10px;
+  }
 
-    background-color: var(--grey-3);
+  .drop-down {
+    position: relative;
+
+  }
+
+  .user-profile {
+    display: flex;
+    align-items: center;
+
+    cursor: pointer;
+
+  }
+
+  .dropdown-menu {
+    position: absolute;
+    top: 100%;
+    right: 0;
+    display: flex;
+    flex-direction: column;
+
+    background-color: #000;
+    box-shadow: 0px 0px 50px rgba(0, 0, 0, 0.2);
+
+    padding-right: 1rem;
+    padding-left: 1rem;
+    
+  }
+
+  .dropdown-item {
+    margin: 0.5rem 0;
+    border: none;
+
+    background-color: transparent;
+    
+    cursor: pointer;
+
+    color: var(--color-primary);
+
+  }
+
+
+    .backPageBtn,
+    .logoutBtn {
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+
+      height: 32px;
+      width: 79.5px;
+
+      border-radius: 4px;
+      padding: 0px 16px 0px 16px;
+
+      color: var(--grey-0);
+      font-size: 10px;
+
+      background-color: var(--grey-3);
+
+    }
+
+  .header-right{
+    display: flex;
+
   }
 `;
